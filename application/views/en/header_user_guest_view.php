702edfe1596a7fd167aaab1fbfe22bd87ea4c06b
Aleksandr Kulykov <olku@isd.dp.ua>
2013-08-29 17:23:00 +0300

    DB

--- application/views/en/header_user_guest_view.php
+++ application/views/en/header_user_guest_view.php
@@ -1,2 +1,2 @@
-<a href="#modal-registration" data-toggle="modal" class="link-registration -dotted">Зарегистрироваться</a>
-<a href="#modal-login" data-toggle="modal" class="btn -special link-login _fright">Войти <i class="icon icon-access"></i></a>
+<a href="#modal-registration" data-toggle="modal" class="link-registration -dotted">Registration</a>
+<a href="#modal-login" data-toggle="modal" class="btn -special link-login _fright">Enter <i class="icon icon-access"></i></a>
